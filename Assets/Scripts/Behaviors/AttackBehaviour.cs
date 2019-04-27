e1b0dbf06bb031e4a351f3b68c53879c3305cf3b
Lvitug <lvitug@maplesmet.org>
2019-04-27 13:58:08 -0500

    Implemented Start and GameOver scene
    Implemented the logic to move between each scenes.

---
+++ Assets/Scripts/Behaviors/AttackBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBehaviour : MonoBehaviour
+{
+    public int damage;
+    public Animator anim;
+    public float cooldownTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
