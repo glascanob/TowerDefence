0a9f3bc0f13440f6cfca461e015369036e49d7d1
Lvitug <lvitug@maplesmet.org>
2019-04-27 13:05:57 -0500

    Implemented PlayerController
    Added inheritance with the player controller and player behaviour

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -2,22 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBehaviour : MonoBehaviour
+public class PlayerBehaviour : PlayerController
 {
-    public int maxHealth;
-    [SerializeField] private int curHealth;
     public int damage;
     public float moveSpeed;
     //public GameObject weapon;
 
     Rigidbody2D rb2D;
-    Attacks spell = Attacks.sword;
     bool enemyInRange = false;
 
     // Use this for initialization
     void Start()
     {
-        curHealth = maxHealth;
         rb2D = GetComponent<Rigidbody2D>();
         //weapon.SetActive(false); // temporary
     }
@@ -25,18 +21,9 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (curHealth > 0)
-        {
-            PlayerMoving();
-            Attack();
-            SetSpell();
-        }
-        else
-        {
-            Debug.Log("YOU DEAD");
-            Destroy(gameObject);
-        }
-        
+        PlayerMoving();
+        Attack();
+        SetSpell();
     }
 
     private void PlayerMoving()
@@ -47,47 +34,17 @@ public class PlayerBehaviour : MonoBehaviour
         rb2D.velocity = new Vector2(hMovement, vMovement);
     }
 
-    public void SetSpell()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            Debug.Log("set to Spell 1!!!");
-            spell = (Attacks) 1;
-        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            Debug.Log("set to Spell 2!!!");
-            spell =  (Attacks) 2;
-        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
-            Debug.Log("set to Spell 3!!!");
-            spell = (Attacks) 3;
-        } else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            Debug.Log("set to Spell 3!!!");
-            spell = (Attacks) 4;
-        }
-    }
-
-    public void SetSpell(int attacks)
-    {
-        spell = (Attacks)attacks;
-    }
-
     GameObject enemy;
-    public void Attack() {
+    public void Attack()
+    {
         if (Input.GetMouseButtonDown(0) && enemyInRange) {
-            //Enable collider for weapon
-            //weapon.SetActive(true); // temporary (after animations are set we can replace this)
-            //trigger some animation
-            Debug.Log("attacking with " + spell.ToString());
+            Debug.Log("attacking with " + attack.ToString());
             enemy.GetComponent<EnemyBehavior>().ReceiveDamage(damage);
         }
         else
         {
             //weapon.SetActive(false);
         }
-    }
-
-    public void Damage(int damage)
-    {
-        curHealth -= damage;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -103,14 +60,4 @@ public class PlayerBehaviour : MonoBehaviour
         enemyInRange = false;
         enemy = null;
     }
-}
-
-public enum Attacks
-{
-    sword, spell1, spell2, spell3, spell4
-}
-
-public enum AttackType
-{
-    heavy, normal, light, air
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Attacks attack = Attacks.sword;
+
+    protected void SetSpell()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("set to Spell 1!!!");
+            attack = (Attacks)1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("set to Spell 2!!!");
+            attack = (Attacks)2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log("set to Spell 3!!!");
+            attack = (Attacks)3;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Debug.Log("set to Spell 3!!!");
+            attack = (Attacks)4;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetSpell(int attacks)
     {
-        
+        attack = (Attacks)attacks;
     }
+}
+
+public enum Attacks
+{
+    sword, spell1, spell2, spell3, spell4
+}
+
+public enum AttackType
+{
+    heavy, normal, light, air
 }
