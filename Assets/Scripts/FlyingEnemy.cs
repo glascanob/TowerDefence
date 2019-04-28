2ae0561b3161eca47b02330346047eb84ee23aad
Gabriel <glascanob@gmail.com>
2019-04-28 14:43:04 -0500

    Adding flying enemy

---
+++ Assets/Scripts/FlyingEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : EnemyBehavior
+{
+    public float cDBetweenAttacks;
+    public override void Update()
+    {
+        base.Update();
+
+        if(attacking)
+        {
+            if(!inCD)
+            {
+                inCD = true;
+                StartCoroutine(base.Attack(cDBetweenAttacks));
+            }
+        }
+    }
+
+    public override void ReceiveDamage(int damage, AttackTypes attackType)
+    {
+        if (attackType == AttackTypes.air)
+        {
+            base.ReceiveDamage(damage, attackType);
+        }
+    }
+}
